5af8484bdbbf46c4cd30f052be387491b94a7521
Blake Collins <blakeac369@gmail.com>
2021-02-23 17:19:45 -0800

    Add files via upload

--- src/Payment.js
+++ src/Payment.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CheckoutProduct from './CheckoutProduct';
 import './Payment.css'
 import { useStateValue } from './StateProvider'
 import { Link } from 'react-router-dom'
 import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+import CurrencyFormat from 'react-currency-format'
+import { getBasketTotal } from './reducer'
 
 function Payment() {
     const [{ basket, user, }, dispatch] = useStateValue();
@@ -12,8 +14,10 @@ function Payment() {
     const stripe = useStripe();
     const elements = useElements();
 
+    const [succeeded, setSucceeded] = useState(false);
+    const [processing, setProcessing] = useState("");
     const [error, setError] = useState(null);
-    const [disable, setDisable] = useState(true);
+    const [disabled, setDisable] = useState(true);
 
     const handleSubmit = e => {
 
@@ -69,6 +73,24 @@ function Payment() {
                     <div className='payment__details'>
                             <form onSubmit={handleSubmit}>
                                 <CardElement onChange={handleChange}/>
+
+                                <div className='payment__priceContainer'>
+                                <CurrencyFormat
+                                    renderText={(value) => (
+                                        <h3>Order Total: {value}</h3>)}
+
+                                    decimalScale={2}
+                                    value={getBasketTotal(basket)}
+                                    displayType={"text"}
+                                    thousandSeparator={true}
+                                    prefix={"$"}
+                                />
+                                <button disabled={processing || disabled || succeeded}>
+                                        <span>{processing ? <p>Processing</p> : "Buy now"}</span>
+                                </button>
+                                </div>
+                                {/*Handle card errors*/}
+                                {error && <div>{error}</div>}
                             </form>
                     </div>
                 </div>
